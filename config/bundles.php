69ab0a956bae47811f84b6e1f0cc10bf9c4cbc7f
kimotoatsumi <atsumi000105@gmail.com>
2019-04-14 22:39:37 -0500

    Add req for symfony profiler
    This will help during debugging in dev mode

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     SamJ\FractalBundle\SamJFractalBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
