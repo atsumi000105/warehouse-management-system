5cc2550acdced562f1d8e25bad834836f86b678a
kimotoatsumi <atsumi000105@gmail.com>
2019-05-27 23:16:58 -0500

    Update phpstan

--- tests/object-manager.php
+++ tests/object-manager.php
@@ -12,7 +12,7 @@ require dirname(__DIR__).'/vendor/autoload.php';
 // The check is to ensure we don't use .env in production
 if (!isset($_SERVER['APP_ENV']) && !isset($_ENV['APP_ENV'])) {
     if (!class_exists(Dotenv::class)) {
-        throw new \RuntimeException(
+        throw new RuntimeException(
             'APP_ENV environment variable is not defined. You need to define environment variables for '
             . 'configuration or add "symfony/dotenv" as a Composer dependency to load variables from a .env file.'
         );
@@ -22,12 +22,6 @@ if (!isset($_SERVER['APP_ENV']) && !isset($_ENV['APP_ENV'])) {
 
 $env = $_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? 'dev';
 $debug = (bool) ($_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? ('prod' !== $env));
-
-if ($debug) {
-    umask(0000);
-
-    Debug::enable();
-}
 
 if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
     Request::setTrustedProxies(
