a5550e761f364ce38753b9e0dbcc00f432e22803
kimotoatsumi <atsumi000105@gmail.com>
2019-07-05 23:52:58 -0500

    Add missing Exception classes

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\LineItem;
 use App\Entity\Order;
+use App\Exception\CommittedTransactionException;
 use App\Transformers\OrderTransformer;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\ParameterBag;
@@ -262,7 +263,7 @@ class OrderController extends BaseController
     protected function checkEditable(Order $order)
     {
         if (!$order->isEditable()) {
-            throw new PermissionDeniedException(
+            throw new CommittedTransactionException(
                 sprintf(
                     'Order %d has committed transactions and cannot be edited. Please enter a correction order.',
                     $order->getId()
