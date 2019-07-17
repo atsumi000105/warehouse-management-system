f1d1d3a1c45da946c7eea7e6716e60f9a4555f82
kimotoatsumi <atsumi000105@gmail.com>
2019-07-17 00:11:52 -0500

    Remove unused exception
    Made superfluous by `Symfony\Component\HttpKernel\Exception\NotFoundHttpException`

--- src/Controller/StorageLocationController.php
+++ src/Controller/StorageLocationController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Entity\InventoryTransaction;
 use App\Entity\StorageLocation;
-use App\Exception\NotFoundApiException;
 use App\Transformers\StorageLocationOptionTransformer;
 use App\Transformers\StorageLocationTransformer;
 use Symfony\Component\HttpFoundation\JsonResponse;
