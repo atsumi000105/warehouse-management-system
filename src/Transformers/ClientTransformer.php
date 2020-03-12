65c9d3c19216b8c26724afbeb849e4b714adfa1a
kimotoatsumi <atsumi000105@gmail.com>
2020-03-11 23:36:02 -0500

    #85 Code style correction

--- src/Transformers/ClientTransformer.php
+++ src/Transformers/ClientTransformer.php
@@ -24,7 +24,8 @@ class ClientTransformer extends TransformerAbstract
             'birthdate' => $client->getBirthdate()->format('c'),
             'isExpirationOverridden' => $client->isExpirationOverridden(),
             'ageExpiresAt' => $client->getAgeExpiresAt()->format('c'),
-            'distributionExpiresAt' => $client->getDistributionExpiresAt() ? $client->getDistributionExpiresAt()->format('c') : null,
+            'distributionExpiresAt' => $client->getDistributionExpiresAt() ?
+                $client->getDistributionExpiresAt()->format('c') : null,
             'pullupDistributionMax' => $client->getPullupDistributionMax(),
             'pullupDistributionCount' => $client->getPullupDistributionCount(),
             'updatedAt' => $client->getUpdatedAt()->format('c'),
