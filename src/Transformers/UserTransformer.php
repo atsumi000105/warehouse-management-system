1971e6bcba45e230e47acb6a1b7a61ef63596828
kimotoatsumi <atsumi000105@gmail.com>
2020-03-12 23:06:33 -0500

    Revert

--- src/Transformers/UserTransformer.php
+++ src/Transformers/UserTransformer.php
@@ -3,14 +3,12 @@
 namespace App\Transformers;
 
 use App\Entity\User;
-use App\Entity\PartnerUser;
 use League\Fractal\TransformerAbstract;
 
 class UserTransformer extends TransformerAbstract
 {
     protected $availableIncludes = [
         'groups',
-        'partners',
     ];
 
     public function transform(User $user)
@@ -31,14 +29,4 @@ class UserTransformer extends TransformerAbstract
         return $this->collection($user->getGroups(), new GroupTransformer());
     }
 
-    public function includePartners(User $user)
-    {
-        if ($user->getPartners ?? false) {
-            $partnerUser = new PartnerUser($user->getEmail());
-            $partners = $partnerUser->getPartners();
-        } else {
-            $partners = [];
-        }
-        return $this->collection($partners, new PartnerTransformer());
-   }
 }
