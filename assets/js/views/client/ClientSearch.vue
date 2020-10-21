e0ec8a038712f618679017abd934368814496d4b
kimotoatsumi <atsumi000105@gmail.com>
2020-10-21 11:30:04 -0500

    #226 Fix field spacing

--- assets/js/views/client/ClientSearch.vue
+++ assets/js/views/client/ClientSearch.vue
@@ -5,13 +5,13 @@
         </h3>
 
         <div class="row">
-            <div class="col-xs-2">
+            <div class="col-xs-3">
                 <TextField
                     v-model="filters.keyword"
                     label="Name (Client and Parent/Guardian)"
                 />
             </div>
-            <div class="col-xs-1">
+            <div class="col-xs-2">
                 <DateField
                     v-model="filters.birthdate"
                     label="Birthdate"
