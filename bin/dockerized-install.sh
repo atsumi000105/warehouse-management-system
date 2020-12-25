eb40815d4b21dfab8afda198e97a6bd2cf21278e
kimotoatsumi <atsumi000105@gmail.com>
2020-12-25 17:43:46 -0600

    Dynamically replace Database URL for dockerized db

--- bin/dockerized-install.sh
+++ bin/dockerized-install.sh
@@ -2,7 +2,16 @@
 
 rm .env.local
 rm .env.local.php
-cp .env.docker .env.local
+cp .env .env.local
+
+# Replace db line with dockerized db
+DB_URL="DATABASE_URL=postgres://postgres:coverd@db/coverd?charset=UTF-8"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # MacOS `sed` needs a zero-length extension
+    sed -i '' -e "s~^DATABASE_URL=postgres.*~$DB_URL~g" .env.local
+else
+    sed -i -e "s~^DATABASE_URL=postgres.*~$DB_URL~g" .env.local
+fi
 
 docker-compose -f docker-compose.yml -f docker-compose.unison.yml down -v --rmi all --remove-orphans
 docker-compose -f docker-compose.yml -f docker-compose.unison.yml up --detach --build
