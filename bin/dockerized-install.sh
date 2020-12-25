f8a33f8a2b2d5fee0f06db54248e568a53fb2f9b
kimotoatsumi <atsumi000105@gmail.com>
2020-12-25 16:35:10 -0600

    Bugfix - nuclear install now completes

--- bin/dockerized-install.sh
+++ bin/dockerized-install.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 rm .env.local
+rm .env.local.php
 cp .env.docker .env.local
 
-docker-compose -f docker-compose.yml -f docker-compose.unison.yml down -v --rmi all --remove-orphans /
- && docker-compose -f docker-compose.yml -f docker-compose.unison.yml up --detach --build
+docker-compose -f docker-compose.yml -f docker-compose.unison.yml down -v --rmi all --remove-orphans
+docker-compose -f docker-compose.yml -f docker-compose.unison.yml up --detach --build
 
 ./docker/app ./bin/php_install.sh
 ./docker/app ./bin/js_install.sh
