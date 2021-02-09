30ff6d5f24a10aff84d379f706e88edb850e77ec
kimotoatsumi <atsumi000105@gmail.com>
2021-02-09 00:37:57 -0600

    Alphasort workflows

--- config/packages/workflow.php
+++ config/packages/workflow.php
@@ -5,6 +5,99 @@ use App\Entity\Partner;
 
 $container->loadFromExtension('framework', [
     'workflows' => [
+        'client_management' => [
+            'type' => 'state_machine',
+            'audit_trail' => [
+                'enabled' => true,
+            ],
+            'marking_store' => [
+                'type' => 'method',
+                'property' => 'status',
+            ],
+            'supports' => [
+                Client::class,
+            ],
+            'initial_marking' => Client::STATUS_CREATION,
+            'places' => [
+                Client::STATUS_ACTIVE,
+                Client::STATUS_CREATION,
+                Client::STATUS_INACTIVE,
+                Client::STATUS_INACTIVE_DUPLICATE,
+                Client::STATUS_INACTIVE_EXPIRED,
+                Client::STATUS_NEEDS_REVIEW,
+                Client::STATUS_REVIEW_PAST_DUE,
+            ],
+            'transitions' => [
+                Client::TRANSITION_ACTIVATE => [
+                    'metadata' => [
+                        'title' => 'Activate'
+                    ],
+                    'from' => [
+                        Client::STATUS_CREATION,
+                        Client::STATUS_INACTIVE,
+                        Client::STATUS_INACTIVE_DUPLICATE,
+                        Client::STATUS_INACTIVE_EXPIRED,
+                        Client::STATUS_NEEDS_REVIEW,
+                        Client::STATUS_REVIEW_PAST_DUE,
+                    ],
+                    'to' => Client::STATUS_ACTIVE,
+                ],
+                Client::TRANSITION_DEACTIVATE => [
+                    'metadata' => [
+                        'title' => 'Deactivate'
+                    ],
+                    'from' => [
+                        Client::STATUS_ACTIVE,
+                        Client::STATUS_CREATION,
+                        Client::STATUS_INACTIVE_DUPLICATE,
+                        Client::STATUS_INACTIVE_EXPIRED,
+                    ],
+                    'to' => Client::STATUS_INACTIVE,
+                ],
+                Client::TRANSITION_DEACTIVATE_EXPIRE => [
+                    'metadata' => [
+                        'title' => 'Deactivate (Expire Client)'
+                    ],
+                    'from' => [
+                        Client::STATUS_ACTIVE,
+                        Client::STATUS_CREATION,
+                        Client::STATUS_INACTIVE,
+                        Client::STATUS_INACTIVE_DUPLICATE,
+                    ],
+                    'to' => Client::STATUS_INACTIVE_EXPIRED,
+                ],
+                Client::TRANSITION_DEACTIVATE_DUPLICATE => [
+                    'metadata' => [
+                        'title' => 'Deactivate (Duplicate)'
+                    ],
+                    'from' => [
+                        Client::STATUS_ACTIVE,
+                        Client::STATUS_CREATION,
+                        Client::STATUS_INACTIVE,
+                        Client::STATUS_INACTIVE_EXPIRED,
+                    ],
+                    'to' => Client::STATUS_INACTIVE_DUPLICATE,
+                ],
+                Client::TRANSITION_FLAG_FOR_REVIEW => [
+                    'metadata' => [
+                        'title' => 'Needs Review'
+                    ],
+                    'from' => [
+                        Client::STATUS_ACTIVE,
+                    ],
+                    'to' => Client::STATUS_NEEDS_REVIEW,
+                ],
+                Client::TRANSITION_FLAG_FOR_REVIEW_PAST_DUE => [
+                    'metadata' => [
+                        'title' => 'Review Past Due'
+                    ],
+                    'from' => [
+                        Client::STATUS_NEEDS_REVIEW,
+                    ],
+                    'to' => Client::STATUS_REVIEW_PAST_DUE,
+                ],
+            ],
+        ],
         'partner_management' => [
             'type' => 'state_machine',
             'audit_trail' => [
@@ -110,99 +203,6 @@ $container->loadFromExtension('framework', [
                         Partner::STATUS_REVIEW_PAST_DUE,
                     ],
                     'to' => Partner::STATUS_INACTIVE,
-                ],
-            ],
-        ],
-        'client_management' => [
-            'type' => 'state_machine',
-            'audit_trail' => [
-                'enabled' => true,
-            ],
-            'marking_store' => [
-                'type' => 'method',
-                'property' => 'status',
-            ],
-            'supports' => [
-                Client::class,
-            ],
-            'initial_marking' => Client::STATUS_CREATION,
-            'places' => [
-                Client::STATUS_ACTIVE,
-                Client::STATUS_CREATION,
-                Client::STATUS_INACTIVE,
-                Client::STATUS_INACTIVE_DUPLICATE,
-                Client::STATUS_INACTIVE_EXPIRED,
-                Client::STATUS_NEEDS_REVIEW,
-                Client::STATUS_REVIEW_PAST_DUE,
-            ],
-            'transitions' => [
-                Client::TRANSITION_ACTIVATE => [
-                    'metadata' => [
-                        'title' => 'Activate'
-                    ],
-                    'from' => [
-                        Client::STATUS_CREATION,
-                        Client::STATUS_INACTIVE,
-                        Client::STATUS_INACTIVE_DUPLICATE,
-                        Client::STATUS_INACTIVE_EXPIRED,
-                        Client::STATUS_NEEDS_REVIEW,
-                        Client::STATUS_REVIEW_PAST_DUE,
-                    ],
-                    'to' => Client::STATUS_ACTIVE,
-                ],
-                Client::TRANSITION_DEACTIVATE => [
-                    'metadata' => [
-                        'title' => 'Deactivate'
-                    ],
-                    'from' => [
-                        Client::STATUS_ACTIVE,
-                        Client::STATUS_CREATION,
-                        Client::STATUS_INACTIVE_DUPLICATE,
-                        Client::STATUS_INACTIVE_EXPIRED,
-                    ],
-                    'to' => Client::STATUS_INACTIVE,
-                ],
-                Client::TRANSITION_DEACTIVATE_EXPIRE => [
-                    'metadata' => [
-                        'title' => 'Deactivate (Expire Client)'
-                    ],
-                    'from' => [
-                        Client::STATUS_ACTIVE,
-                        Client::STATUS_CREATION,
-                        Client::STATUS_INACTIVE,
-                        Client::STATUS_INACTIVE_DUPLICATE,
-                    ],
-                    'to' => Client::STATUS_INACTIVE_EXPIRED,
-                ],
-                Client::TRANSITION_DEACTIVATE_DUPLICATE => [
-                    'metadata' => [
-                        'title' => 'Deactivate (Duplicate)'
-                    ],
-                    'from' => [
-                        Client::STATUS_ACTIVE,
-                        Client::STATUS_CREATION,
-                        Client::STATUS_INACTIVE,
-                        Client::STATUS_INACTIVE_EXPIRED,
-                    ],
-                    'to' => Client::STATUS_INACTIVE_DUPLICATE,
-                ],
-                Client::TRANSITION_FLAG_FOR_REVIEW => [
-                    'metadata' => [
-                        'title' => 'Needs Review'
-                    ],
-                    'from' => [
-                        Client::STATUS_ACTIVE,
-                    ],
-                    'to' => Client::STATUS_NEEDS_REVIEW,
-                ],
-                Client::TRANSITION_FLAG_FOR_REVIEW_PAST_DUE => [
-                    'metadata' => [
-                        'title' => 'Review Past Due'
-                    ],
-                    'from' => [
-                        Client::STATUS_NEEDS_REVIEW,
-                    ],
-                    'to' => Client::STATUS_REVIEW_PAST_DUE,
                 ],
             ],
         ],
