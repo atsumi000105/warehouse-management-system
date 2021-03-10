c10cd29856f429d4e4a0852b0d29b17962b2d168
kimotoatsumi <atsumi000105@gmail.com>
2021-03-09 19:14:32 -0600

    #238 WIP remove client listener mailer code for now.

--- src/Listener/ClientListener.php
+++ src/Listener/ClientListener.php
@@ -9,8 +9,6 @@ use Doctrine\Common\EventSubscriber;
 use Doctrine\ORM\Event\LifecycleEventArgs;
 use Doctrine\ORM\Event\PreUpdateEventArgs;
 use Doctrine\ORM\Events;
-use Symfony\Bridge\Twig\Mime\TemplatedEmail;
-use Symfony\Component\Mailer\MailerInterface;
 
 class ClientListener implements EventSubscriber
 {
@@ -19,15 +17,9 @@ class ClientListener implements EventSubscriber
      */
     protected $gen;
 
-    /**
-     * @var MailerInterface
-     */
-    protected $mailer;
-
-    public function __construct(RandomIdGenerator $gen, MailerInterface $mailer)
+    public function __construct(RandomIdGenerator $gen)
     {
         $this->gen = $gen;
-        $this->mailer = $mailer;
     }
 
     public function postLoad(LifecycleEventArgs $event)
@@ -67,20 +59,6 @@ class ClientListener implements EventSubscriber
         if (!$client instanceof Client) {
             return;
         }
-
-        $email = (new TemplatedEmail())
-            ->addFrom('andrew@koebbe.com')
-            ->addTo('andrew@koebbe.com')
-            ->subject('Order Updated')
-            ->htmlTemplate('emails/order-update.html.twig')
-            ->text('Something here')
-            ->context([
-                'client' => $client,
-                'partner' => $client->getPartner(),
-                'contact' =>  $client->getPartner()->getContacts()->first()
-            ]);
-
-        $this->mailer->send($email);
 
         $client->calculateAgeExpiration();
     }
