854494bb5f7ea4b776c22ad087be65bf6aeb9195
kimotoatsumi <atsumi000105@gmail.com>
2021-03-16 19:42:16 +0000

    typo

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -19,7 +19,7 @@ wget https://composer.github.io/installer.sig -O - -q | tr -d '\n' > installer.s
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('SHA384', 'composer-setup.php') === file_get_contents('installer.sig')) { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php --install-dir=/usr/local/bin --filename=composer
-chmoe +x /usr/local/bin/composer
+chmod +x /usr/local/bin/composer
 php -r "unlink('composer-setup.php'); unlink('installer.sig');"
 composer self-update --2
 composer install --no-interaction
