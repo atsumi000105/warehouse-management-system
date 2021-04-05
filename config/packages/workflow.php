e9505d901e6c86a19f53f94e6c7539925df2a9e3
kimotoatsumi <atsumi000105@gmail.com>
2021-04-05 08:58:51 -0500

    Create PartnerOrder Workflow

--- config/packages/workflow.php
+++ config/packages/workflow.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Entity\Client;
+use App\Entity\Orders\PartnerOrder;
 use App\Entity\Partner;
 
 $container->loadFromExtension('framework', [
@@ -221,6 +222,60 @@ $container->loadFromExtension('framework', [
                         Partner::STATUS_REVIEW_PAST_DUE,
                     ],
                     'to' => Partner::STATUS_INACTIVE,
+                ],
+            ],
+        ],
+        'partner_order' => [
+            'type' => 'state_machine',
+            'audit_trail' => [
+                'enabled' => true,
+            ],
+            'marking_store' => [
+                'type' => 'method',
+                'property' => 'status',
+            ],
+            'supports' => [
+                PartnerOrder::class,
+            ],
+            'initial_marking' => PartnerOrder::STATUS_CREATING,
+            'places' => PartnerOrder::STATUSES,
+            'transitions' => [
+                PartnerOrder::TRANSITION_SUBMIT => [
+                    'metadata' => [
+                        'title' => 'Submit',
+                    ],
+                    'from' => [
+                        PartnerOrder::STATUS_CREATING,
+                    ],
+                    'to' => PartnerOrder::STATUS_SUBMITTED,
+                ],
+                PartnerOrder::TRANSITION_ACCEPT => [
+                    'metadata' => [
+                        'title' => 'Accept',
+                    ],
+                    'from' => [
+                        PartnerOrder::STATUS_COMPLETED,
+                        PartnerOrder::STATUS_SUBMITTED,
+                    ],
+                    'to' => PartnerOrder::STATUS_IN_PROCESS,
+                ],
+                PartnerOrder::TRANSITION_COMPLETE => [
+                    'metadata' => [
+                        'title' => 'Ship'
+                    ],
+                    'from' => [
+                        PartnerOrder::STATUS_IN_PROCESS,
+                    ],
+                    'to' => PartnerOrder::STATUS_COMPLETED,
+                ],
+                PartnerOrder::TRANSITION_REVERT_TO_CREATING => [
+                    'metadata' => [
+                        'title' => 'Revert To Creating'
+                    ],
+                    'from' => [
+                        PartnerOrder::STATUS_IN_PROCESS,
+                    ],
+                    'to' => PartnerOrder::STATUS_CREATING,
                 ],
             ],
         ],
