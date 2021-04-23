5557dacd580cd39627312d25c4727fd1a430936a
kimotoatsumi <atsumi000105@gmail.com>
2021-04-23 23:17:42 +0300

    Make the product list table paged

--- assets/js/views/product/ProductList.vue
+++ assets/js/views/product/ProductList.vue
@@ -43,52 +43,13 @@
                     </div>
                     <!-- /.box-header -->
                     <div class="box-body table-responsive no-padding">
-                        <div
-                            v-if="loading"
-                            class="loadingArea"
-                        >
-                            <pulse-loader
-                                :loading="loading"
-                                color="#3c8dbc"
-                            />
-                        </div>
-                        <table
-                            v-else
-                            class="table table-hover"
-                        >
-                            <thead>
-                                <tr>
-                                    <th />
-                                    <th>Product ID</th>
-                                    <th>Name</th>
-                                    <th>Category</th>
-                                    <th>Order</th>
-                                    <th>Status</th>
-                                    <th>Last Updated</th>
-                                </tr>
-                            </thead>
-                            <Draggable
-                                v-model="products.data"
-                                tag="tbody"
-                            >
-                                <tr
-                                    v-for="product in products.data"
-                                    :key="product.id"
-                                >
-                                    <td><i class="fa fa-arrows" /></td>
-                                    <td>
-                                        <router-link :to="{ name: 'product-edit', params: { id: product.id }}">
-                                            <i class="fa fa-edit" />{{ product.id }}
-                                        </router-link>
-                                    </td>
-                                    <td v-text="product.name" />
-                                    <td v-text="product.productCategory.name" />
-                                    <td v-text="product.orderIndex" />
-                                    <td v-text="product.status" />
-                                    <td>{{ product.updatedAt | dateTimeFormat }}</td>
-                                </tr>
-                            </Draggable>
-                        </table>
+                        <TablePaged
+                            :columns="columns"
+                            api-url="/api/products"
+                            edit-route="/products/"
+                            :sort-order="[{ field: 'id', direction: 'desc' }]"
+                            :per-page="50"
+                        />
                     </div>
                     <!-- /.box-body -->
                 </div>
@@ -99,43 +60,33 @@
 </template>
 
 <script>
-    import PulseLoader from "vue-spinner/src/PulseLoader";
-    import Draggable from 'vuedraggable';
+    import TablePaged from "../../components/TablePaged";
 
     export default {
         components: {
-            PulseLoader,
-            Draggable,
+            TablePaged
         },
         props:[],
         data() {
             return {
-                products: {},
                 loading: true,
+                columns: [
+                    { name: "__slot:link", title: "Product ID", sortField: "id" },
+                    { name: "name", title: "Name", sortField: "name" },
+                    { name: "productCategory.name", title: "Category", sortField: "productCategory.name" },
+                    { name: "orderIndex", title: "Order", sortField: "orderIndex" },
+                    { name: "status", title: "Status", sortField: "status" },
+                    { name: "updatedAt", title: "Last Updated", callback: "dateTimeFormat", sortField: "updatedAt" }
+                ]
             };
         },
         created() {
-            axios
-                .get('/api/products')
-                .then(response => this.products = response.data).catch(error => {
-                    console.log(error)
-                })
-                .finally(() => this.loading = false);
             console.log('Component mounted.');
         },
         methods: {
             saveSort() {
-                let me = this;
-                let ids = this.products.data.map(product => product.id);
-                axios
-                    .post('/api/products/order', {
-                        ids: ids,
-                    })
-                    .then(response => me.setProducts(response.data.data));
+                // TO DO:
             },
-            setProducts(products) {
-                this.products.data = products.sort((a, b) => a.orderIndex - b.orderIndex);
-            }
         }
     }
 </script>
