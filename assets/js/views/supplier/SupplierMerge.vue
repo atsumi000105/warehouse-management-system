f8f9d13a64783ed78fce9e8ed7aa8f8d76fdd2a7
kimotoatsumi <atsumi000105@gmail.com>
2021-04-27 23:12:05 +0300

    Supplier merge not working

--- assets/js/views/supplier/SupplierMerge.vue
+++ assets/js/views/supplier/SupplierMerge.vue
@@ -131,6 +131,11 @@
         methods: {
             mergeSuppliers: function() {
                 let me = this;
+                console.log({
+                        targetSupplier: this.targetSupplier.id,
+                        sourceSuppliers: this.getSourcesSupplierIds(),
+                        context: this.mergeContext,
+                    })
                 axios
                     .post('/api/suppliers/merge', {
                         targetSupplier: this.targetSupplier.id,
