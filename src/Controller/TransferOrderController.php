23556f5116b130d48679610f1370999960cebc8e
kimotoatsumi <atsumi000105@gmail.com>
2021-06-14 21:43:35 -0600

    removed whitespace

--- src/Controller/TransferOrderController.php
+++ src/Controller/TransferOrderController.php
@@ -134,7 +134,7 @@ class TransferOrderController extends BaseOrderController
     {
         return TransferOrderVoter::VIEW;
     }
-    
+
     /**
      * @param Request $request
      * @return ParameterBag
