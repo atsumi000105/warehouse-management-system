90a6a8caef54ea64d68078aff595bc50a760c964
kimotoatsumi <atsumi000105@gmail.com>
2021-10-21 21:53:39 -0500

    Add Client name/birthdate backend validations

--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -11,6 +11,7 @@ use Gedmo\Mapping\Annotation as Gedmo;
 use Moment\Moment;
 use Symfony\Component\Routing\Exception\MissingMandatoryParametersException;
 use Symfony\Component\Workflow\Registry;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ClientRepository")
@@ -70,6 +71,10 @@ class Client extends CoreEntity implements AttributedEntityInterface
      *
      * @ORM\Embedded(class="App\Entity\ValueObjects\Name", columnPrefix=false)
      * @Gedmo\Versioned
+     * @Assert\Expression(
+     *     "this.getName().hasFirstAndLastName()",
+     *     message="Please type a first and last name."
+     * )
      */
     protected $name;
 
@@ -85,6 +90,8 @@ class Client extends CoreEntity implements AttributedEntityInterface
      *
      * @ORM\Column(type="date")
      * @Gedmo\Versioned
+     * @Assert\NotBlank
+     * @Assert\LessThanOrEqual("today")
      */
     protected $birthdate;
 
