38a9c6ef1001a6bec591eaa91e7cf2379d68385c
kimotoatsumi <atsumi000105@gmail.com>
2021-11-04 01:12:44 -0500

    #251 Update pipeline to use php 7.4

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -9,7 +9,6 @@ set -xe
 apt-get update -yqq
 apt-get install git wget libfreetype6-dev libjpeg62-turbo-dev libpng-dev libzip-dev libpq-dev libsodium-dev libxslt1-dev -yqq
 
-docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/
 docker-php-ext-install gd
 docker-php-ext-install zip
 docker-php-ext-install xsl
