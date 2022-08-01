0099d19827d74790213a3bb887a42deab2b1170c
kimotoatsumi <atsumi000105@gmail.com>
2022-08-01 14:50:16 -0400

    PartnerList.vue partner filtering not working fixed

--- assets/js/views/partner/PartnerList.vue
+++ assets/js/views/partner/PartnerList.vue
@@ -11,13 +11,11 @@
         </h3>
         <div class="row">
             <div class="col-xs-2">
-                <option-list
-                    v-model="filters"
-                    label="Partner Name"
-                    api-path="partners"
-                    property="id"
-                    display-property="title"
-                    empty-string="-- All Partners --"
+                <PartnerSelectionForm
+                    v-model="filters.partner"
+                    label="Partner"
+                    :options="allPartners"
+                    @partner-change="updatePartner"
                 />
             </div>
             <div class="col-xs-2">
@@ -87,45 +85,56 @@
 import TablePaged from '../../components/TablePaged.vue';
 import OptionListStatic from '../../components/OptionListStatic.vue';
 import OptionList from '../../components/OptionListEntity.vue';
+import PartnerSelectionForm from "../../components/PartnerSelectionForm";
+import {mapGetters} from "vuex";
 
 export default {
     components: {
         TablePaged,
         OptionListStatic,
-        OptionList
+        OptionList,
+        PartnerSelectionForm
         },
-        data() {
-            return {
-                columns: [
-                    { name: '__checkbox', title: "#" },
-                    { name: '__slot:link', title: "Partner Id", sortField: 'id' },
-                    { name: 'title', title: "Title", sortField: 'title' },
-                    { name: 'partnerType', title: "Type", sortField: 'p.partnerType' },
-                    { name: 'status', title: "Status", sortField: 'status' },
-                    { name: 'fulfillmentPeriod.name', title: "Fulfillment Period", sortField: 'fulfillmentPeriod' },
-                    { name: 'distributionMethod.name', title: "Distribution Method", sortField: 'distributionMethod' },
-                    { name: 'updatedAt', title: "Last Updated", callback: 'dateTimeFormat', sortField: 'updatedAt' },
-                ],
-                filters: {
-                    status: null,
-                    id: null,
-                    partnerType: null,
-                    fulfillmentPeriod: null,
-                    distributionMethod: null
-                },
-                statuses: [
-                    { id: "ACTIVE", name: "Active" },
-                    { id: "APPLICATION_PENDING_PRIORITY", name: "Application Pending Priority" },
-                    { id: "INACTIVE", name: "Inactive" },
-                    { id: "START", name: "Start" },
-                ],
-                types: [
-                    { id: "HOSPITAL", name: "Hospital" },
-                    { id: "AGENCY", name: "Agency" },
-                ],
-            };
-        },
+
+    data() {
+        return {
+            columns: [
+                { name: '__checkbox', title: "#" },
+                { name: '__slot:link', title: "Partner Id", sortField: 'id' },
+                { name: 'title', title: "Title", sortField: 'title' },
+                { name: 'partnerType', title: "Type", sortField: 'p.partnerType' },
+                { name: 'status', title: "Status", sortField: 'status' },
+                { name: 'fulfillmentPeriod.name', title: "Fulfillment Period", sortField: 'fulfillmentPeriod' },
+                { name: 'distributionMethod.name', title: "Distribution Method", sortField: 'distributionMethod' },
+                { name: 'updatedAt', title: "Last Updated", callback: 'dateTimeFormat', sortField: 'updatedAt' },
+            ],
+            filters: {
+                status: null,
+                id: null,
+                partnerType: null,
+                fulfillmentPeriod: null,
+                distributionMethod: null,
+                partner: {},
+            },
+            statuses: [
+                { id: "ACTIVE", name: "Active" },
+                { id: "APPLICATION_PENDING_PRIORITY", name: "Application Pending Priority" },
+                { id: "INACTIVE", name: "Inactive" },
+                { id: "START", name: "Start" },
+            ],
+            types: [
+                { id: "HOSPITAL", name: "Hospital" },
+                { id: "AGENCY", name: "Agency" },
+            ],
+        };
+    },
+
+    computed: mapGetters(["allPartners"]),
+
     methods: {
+        updatePartner(partner) {
+            this.filters.id = partner.id;
+        },
         doFilter () {
             console.log('doFilter:', this.requestParams(), this.filters);
             this.$events.fire('filter-set', this.requestParams());
